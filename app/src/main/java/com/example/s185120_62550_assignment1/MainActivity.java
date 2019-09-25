267113e301c648f61de3c69c3ee8f551629dddea
DAFA <s185120@student.dtu.dk>
2019-09-26 00:55:46 +0200

    Mangler bare URL vinduet

--- app/src/main/java/com/example/s185120_62550_assignment1/MainActivity.java
+++ app/src/main/java/com/example/s185120_62550_assignment1/MainActivity.java
@@ -11,8 +11,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView nameText = null;
+    private TextView statusText = null;
     private EditText nameField = null;
     private Button nameButton = null;
+    private Button settingsButton = null;
+    private Button guidanceButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +24,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nameText = findViewById(R.id.nameText);
         nameField = findViewById(R.id.nameField);
         nameButton = findViewById(R.id.nameButton);
+        nameText.setOnClickListener(this);
+        nameField.setOnClickListener(this);
+        nameButton.setOnClickListener(this);
 
-
+        statusText = findViewById(R.id.textStatus);
+        settingsButton = findViewById(R.id.buttonSettings);
+        guidanceButton = findViewById(R.id.buttonGuidance);
+        settingsButton.setOnClickListener(this);
+        guidanceButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         if (v == nameText) {
-            System.out.println("[INFO] User pressed textview: " + nameText.getId());
+            System.out.println("[INFO] User pressed textview: " + nameText.getResources().getResourceName(nameText.getId()));
             nameText.setText("Hi, this is the textview");
         } else if (v == nameField) {
-            System.out.println("[INFO] User pressed exittext: " + nameText.getId());
+            System.out.println("[INFO] User pressed exittext: " + nameField.getResources().getResourceName(nameField.getId()));
         } else if (v == nameButton) {
-            System.out.println("[INFO] User pressed button: " + nameText.getId());
+            System.out.println("[INFO] User pressed button: " + nameButton.getResources().getResourceName(nameButton.getId()));
+            statusText.setText("Status: user pressed 'send name' button");
             if (!nameField.isEnabled()) {
                 nameText.setText("Sure, '" + nameField.getText().toString() + "'");
                 nameField.setEnabled(true);
                 System.out.println("[INFO] Program state: sassy");
-            } else if (nameField.getText().toString().equals("")) {
-                nameText.setText("Please enter your name");
+            } else if (nameField.getText().toString().isEmpty()) {
+                nameText.setText("Please enter your name ...");
                 System.out.println("[INFO] Program state: impatient");
             } else {
-                nameText.setText("Is your name really " + nameField.getText().toString() + "?");
-                nameField.setText("");
+                nameText.setText("Really? Your name is " + nameField.getText().toString() + "?");
                 nameField.setEnabled(false);
                 nameButton.setText("Just accept it");
                 System.out.println("[INFO] Program state: doubtful");
             }
+        } else if (v == guidanceButton) {
+            System.out.println("[INFO] User pressed button: " + guidanceButton.getResources().getResourceName(guidanceButton.getId()));
+            statusText.setText("Status: user pressed 'guidance' button");
+        } else if (v == settingsButton) {
+            System.out.println("[INFO] User pressed button: " + settingsButton.getResources().getResourceName(settingsButton.getId()));
+            statusText.setText("Status: user pressed 'settings' button");
         }
 
     }
